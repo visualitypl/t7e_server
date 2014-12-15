84de26539040f77599cb9b240673be4a3be14df1
Marcin Prokop <marcin@prokop.co>
2014-12-15 12:43:04 +0100

    Add link google translate helper
    Example usage:
            <%= link_google_translate('car', 'en', 'pl') %>

--- app/helpers/translations_helper.rb
+++ app/helpers/translations_helper.rb
@@ -1,2 +1,5 @@
 module TranslationsHelper
+  def link_google_translate(text, from_language, to_language)
+    "https://translate.google.pl/?ie=UTF-8##{from_language}/#{to_language}/#{text}"
+  end
 end
