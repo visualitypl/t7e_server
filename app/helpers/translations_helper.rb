315560b509ce15ecfa9ce2feabae55bd0d9180d5
Marcin Prokop <marcin@prokop.co>
2014-12-15 16:38:30 +0100

    Improve link_google_translate helper

--- app/helpers/translations_helper.rb
+++ app/helpers/translations_helper.rb
@@ -1,5 +1,8 @@
 module TranslationsHelper
-  def link_google_translate(text, from_language, to_language)
-    "https://translate.google.pl/?ie=UTF-8##{from_language}/#{to_language}/#{text}"
+  def link_google_translate(params = {}, html_options = {})
+    url  = "https://translate.google.pl/?ie=UTF-8##{params[:from_language]}/#{params[:to_language]}/#{params[:text]}"
+    name = params[:name] || params[:text] # default link name is text to translate
+
+    link_to(name, url, html_options)
   end
 end
