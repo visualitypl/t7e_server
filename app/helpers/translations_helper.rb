c6007b260361eed5434ae34b39cb4cca59e92468
Sakir Temel <sakirtemel@gmail.com>
2014-12-16 13:57:02 +0100

    - link_to_google_translate modify

--- app/helpers/translations_helper.rb
+++ app/helpers/translations_helper.rb
@@ -1,8 +1,16 @@
 module TranslationsHelper
-  def link_google_translate(params = {}, html_options = {})
-    url  = "https://translate.google.pl/?ie=UTF-8##{params[:from_language]}/#{params[:to_language]}/#{params[:text]}"
-    name = params[:name] || params[:text] # default link name is text to translate
+  #= link_to_google_translate 'Translate on Google', from_language: 'pl', to_language: 'en', text: 'Jeden', target: '_blank'
+  def link_to_google_translate(name, options = {})
+    raise "Must pass a hash containing 'from_language'" if not options.is_a?(Hash) or not options.has_key?(:from_language)
+    raise "Must pass a hash containing 'to_language'" if not options.is_a?(Hash) or not options.has_key?(:to_language)
+    raise "Must pass a hash containing 'text'" if not options.is_a?(Hash) or not options.has_key?(:text)
 
-    link_to(name, url, html_options)
+    url  = "https://translate.google.pl/?ie=UTF-8##{options[:from_language]}/#{options[:to_language]}/#{options[:text]}"
+
+    options.delete(:from_language)
+    options.delete(:to_language)
+    options.delete(:text)
+
+    link_to(name, url, options)
   end
 end
