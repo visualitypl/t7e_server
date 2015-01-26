d783a58ff31e2b53c8ad8d15cb8c4de5615cc95a
Sakir Temel <sakirtemel@gmail.com>
2015-01-26 18:35:52 +0100

    - sendgrid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => ENV['T7E_HOST'] }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com'
+  }
 end
