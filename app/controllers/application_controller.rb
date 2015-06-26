a9eecd252277379443aca5558ba0ed2a14faf67f
Sakir Temel <sakirtemel@gmail.com>
2015-06-26 12:08:00 +0200

    Configure pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   before_action :authenticate_user!
   before_action :set_projects
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 
@@ -11,5 +14,10 @@ class ApplicationController < ActionController::Base
     return [] unless current_user
 
     @current_user_projects = (current_user.id == 1) ? Project.all : current_user.projects
+  end
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_to(request.referrer || root_path)
   end
 end
